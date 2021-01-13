c8b2b7cd5cfa3dba9ea9130af8425d1799a17683
shorygina <teshorygina@edu.hse.ru>
2021-01-13 10:04:34 +0300

    Another messy commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,10 +16,9 @@
           <q-badge align="top">&beta;</q-badge>
         </q-toolbar-title>
 
-        <q-space/>
+        <q-space />
 
-
-<!--
+        <!--
         <q-btn flat round dense icon="apps" class="q-mr-sm">
           <q-menu
             :offset="[0, 20]"
@@ -56,14 +55,21 @@
         </q-btn>
 -->
 
-        <q-btn flat round dense icon="notifications" class="q-mr-xs"/>
+        <q-btn flat round dense icon="notifications" class="q-mr-xs" />
 
         <div>
           <q-avatar color="pink" text-color="white">{{ name_abbr }}</q-avatar>
         </div>
 
-        <q-btn flat round dense icon="exit_to_app" @click="logout" v-if="$router.currentRoute.name !== 'Login'"
-               class="q-ml-sm"/>
+        <q-btn
+          flat
+          round
+          dense
+          icon="exit_to_app"
+          @click="logout"
+          v-if="$router.currentRoute.name !== 'Login'"
+          class="q-ml-sm"
+        />
       </q-toolbar>
     </q-header>
 
@@ -74,10 +80,7 @@
       content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
+        <q-item-label header class="text-grey-8">
           Сервисы
         </q-item-label>
         <EssentialLink
@@ -103,19 +106,19 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from "components/EssentialLink.vue";
 
 const commonLinksData = [
   {
-    title: 'Главная',
-    icon: 'home',
-    link: '/',
+    title: "Главная",
+    icon: "home",
+    link: "/",
     exact: true
   },
   // {
@@ -125,12 +128,12 @@ const commonLinksData = [
   //   link: 'https://quasar.dev'
   // },
   {
-    title: 'Онлайн-занятия',
-    caption: 'Выбор платформы для занятий',
-    icon: 'today',
-    link: 'online-platforms',
-    visible: false,
-  },
+    title: "Онлайн-занятия",
+    caption: "Выбор платформы для занятий",
+    icon: "today",
+    link: "online-platforms",
+    visible: false
+  }
 
   // {
   //   title: 'CI/CD грейдеры',
@@ -148,77 +151,84 @@ const commonLinksData = [
 
 const supportLinksData = [
   {
-    title: 'Справка',
-    caption: 'Как пользоваться сервисами',
-    icon: 'help_center',
-    link: ''
+    title: "Справка",
+    caption: "Как пользоваться сервисами",
+    icon: "help_center",
+    link: ""
   },
   {
-    title: 'Поддержка',
-    caption: 'Задать вопрос',
-    icon: 'support',
-    link: ''
+    title: "Поддержка",
+    caption: "Задать вопрос",
+    icon: "support",
+    link: ""
   },
   {
-    title: 'Обновления',
-    caption: 'Новый функционал',
-    icon: 'update',
-    link: ''
+    title: "Обновления",
+    caption: "Новый функционал",
+    icon: "update",
+    link: ""
   },
   {
-    title: 'О сервисе',
-    caption: '',
-    icon: 'info',
-    link: ''
-  },
+    title: "О сервисе",
+    caption: "",
+    icon: "info",
+    link: ""
+  }
 ];
 
 const studentsLinksData = [];
 
 export default {
-  name: 'MainLayout',
-  components: {EssentialLink},
+  name: "MainLayout",
+  components: { EssentialLink },
   data() {
     return {
       leftDrawerOpen: false,
       commonLinks: commonLinksData,
       supportLinks: supportLinksData,
       studentsLinks: studentsLinksData
-    }
+    };
   },
   mounted() {
-    if (this.$q.sessionStorage.getItem('is_teacher')) {
+    if (this.$q.sessionStorage.getItem("is_teacher")) {
       this.commonLinks[1].visible = true;
     }
   },
   computed: {
     name_abbr() {
-      return this.$q.sessionStorage.getItem('name_abbr');
+      return this.$q.sessionStorage.getItem("name_abbr");
     },
     full_name() {
-      return this.$q.sessionStorage.getItem('full_name');
+      return this.$q.sessionStorage.getItem("full_name");
     },
     is_student() {
-      return this.$q.sessionStorage.getItem('is_student');
+      return this.$q.sessionStorage.getItem("is_student");
     },
     is_teacher() {
-      return this.$q.sessionStorage.getItem('is_teacher');
-    },
+      return this.$q.sessionStorage.getItem("is_teacher");
+    }
   },
   methods: {
     logout() {
-      this.$axios.post('/auth/api/v1/auth/logout', {logout: true}, {withCredentials: true}).then((res) => {
-        this.$router.push({name: 'Login'});
-      }).catch((err) => {
-        this.$q.notify({
-          position: this.notificationsPos,
-          icon: 'warning',
-          type: 'negative',
-          multiLine: true,
-          message: "При выполнении выхода произошла ошибка.",
+      this.$axios
+        .post(
+          "/auth/api/v1/auth/logout",
+          { logout: true },
+          { withCredentials: true }
+        )
+        .then(res => {
+          this.$router.push({ name: "Login" });
         })
-      })
+        .catch(err => {
+          this.$q.notify({
+            position: this.notificationsPos,
+            icon: "warning",
+            type: "negative",
+            multiLine: true,
+            message: "При выполнении выхода произошла ошибка."
+          });
+        });
     }
   }
-}
+};
 </script>
