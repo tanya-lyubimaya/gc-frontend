15f9cacb05c7ea42cc441c89729ddd767ef06519
Ilya Korobkov <serverprom@yandex.ru>
2021-01-19 02:12:02 +0300

    :bug: Fix diagrams redirect page path

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
       {path: 'online-platforms', component: () => import('pages/OnlinePlatforms.vue')},
       {path: 'form-create', component: () => import('pages/588/CreateForm.vue')},
       {path: 'feedback', component: () => import('pages/588/Feedback.vue')},
-      {path: 'diagrams-task/с/:c_id/t/:t_id', component: () => import('pages/misc/DiagramsRedirect')},
+      {path: 'diagrams-task/c/:c_id/t/:t_id', component: () => import('pages/misc/DiagramsRedirect')},
       {path: 'ci-cd', component: () => import('pages/589/Index.vue')}
     ]
   },
