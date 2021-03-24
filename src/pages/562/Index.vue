e84c63b86636512c83b8f27d75ee550c05080f0d
teshorygina <teshorygina@edu.hse.ru>
2021-03-25 00:22:32 +0300

    🎉 Added form for hours selection

---
+++ src/pages/562/Index.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-page padding>
+    <div id="q-app">
+      <div class="flex flex-center column">
+        <div class="row" style="min-height: 100%; width: 80%; padding: 24px;">
+          <div
+            id="parent"
+            class="fit row wrap justify-center items-start content-start"
+            style="overflow: hidden;"
+          >
+            <div class="col-10" style="overflow: auto; padding: 30px">
+              <q-card flat bordered style="margin-bottom: 30px">
+                <q-card-section class="bg-primary text-white">
+                  <div class="text-h6">
+                    Дистанционное выполнение лабораторных работ по сетевым
+                    технологиям
+                  </div>
+                  <div class="text-subtitle2">Доступ к онлайн-лаборатории</div>
+                </q-card-section>
+                <div style="padding: 3%">
+                  <q-card-section>
+                    <div style="overflow: auto; width: 40%">
+                      <q-input filled v-model="email" type="email">
+                        <template v-slot:before>
+                          <q-icon name="mail"
+                        /></template>
+                      </q-input>
+                    </div>
+                    <h6 class="text-h6">На сколько часов нужен доступ?</h6>
+                    <q-option-group
+                      v-model="hours"
+                      :options="hours_options"
+                      color="primary"
+                    />
+                  </q-card-section>
+                  <q-card-section>
+                    <div
+                      class="fit row wrap justify-center items-start content-start"
+                    >
+                      <q-btn
+                        label="Отправить"
+                        color="blue"
+                        @click="send(email, hours)"
+                      />
+                    </div>
+                  </q-card-section>
+                </div>
+              </q-card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: this.$q.sessionStorage.getItem("hse_email"),
+      hours: 1,
+      hours_options: [
+        {
+          label: '1',
+          value: '1'
+        },
+        {
+          label: '2',
+          value: '2'
+        },
+        {
+          label: '3',
+          value: '3'
+        },
+        {
+          label: '4',
+          value: '4'
+        }
+      ]
+    };
+  },
+  methods: {
+    send(email, hours) {}
+  }
+};
+</script>
