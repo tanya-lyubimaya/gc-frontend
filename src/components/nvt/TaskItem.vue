52a76bbdcd6468e9be0269c828ea7f9b614e6b2e
Ilya Korobkov <serverprom@yandex.ru>
2021-04-10 02:20:03 +0300

    Update NVT page

---
+++ src/components/nvt/TaskItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-item clickable v-ripple>
+    <q-item-section avatar>
+      <q-avatar :color="avatarColor" text-color="white"
+                :icon="custom_icon ? custom_icon : (passed ? 'check' : 'report_problem')"/>
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label lines="1">{{ title }}</q-item-label>
+      <q-item-label caption lines="1">
+        {{ description }}
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section side>
+      <q-chip square>
+        <span class="text-subtitle" v-if="(type === 'classroom') && passed && !custom_chip">{{ mark }}/{{ max_mark }}</span>
+        <span class="text-subtitle" v-if="(type === 'classroom') && !passed && !custom_chip">не сдано</span>
+        <span class="text-subtitle" v-if="(type === 'taiga') && !custom_chip">{{ mark }} {{ markTitle }}</span>
+        <span class="text-subtitle" v-if="custom_chip">{{ custom_chip }}</span>
+      </q-chip>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  computed: {
+    markTitle() {
+      return this.getTitle(this.mark, ["баллов", "балл", "баллов"])
+    },
+    avatarColor() {
+      switch (this.type) {
+        case "classroom":
+          return this.passed ? 'green' : 'red';
+        case "taiga":
+          return (this.mark > 0) ? 'green' : 'orange';
+        default:
+          return 'grey';
+      }
+    }
+  },
+  props: {
+    title: String,
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    description: String,
+    mark: Number,
+    max_mark: Number,
+    passed: Boolean,
+    type: String,
+    custom_chip: {
+      type: String,
+      default: null
+    },
+    custom_icon: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    getTitle(number, titles) {
+      const cases = [2, 0, 1, 1, 1, 2];
+      return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    }
+  }
+}
+</script>
--- src/pages/nvt/Index.vue
+++ src/pages/nvt/Index.vue
@@ -5,7 +5,6 @@
         <q-card>
           <q-img
             src="~assets/nvt-header-parallax.jpg"
-            basic
           >
             <div class="absolute-bottom text-h5">
               Сетевые видеотехнологии
@@ -14,31 +13,45 @@
           <q-card-section>
             <q-list bordered class="rounded-borders fit">
               <q-item-label header>Оценки за разделы курса</q-item-label>
-
-              <q-item clickable v-ripple>
-                <q-item-section avatar>
-                  <q-avatar color="red" text-color="white" icon="movie_creation"/>
-                </q-item-section>
-
-                <q-item-section>
-                  <q-item-label lines="1">FFMPEG</q-item-label>
-                  <q-item-label caption lines="1">
-                    4 лабораторные работы по ffmpeg
-                  </q-item-label>
-                </q-item-section>
-
-                <q-item-section side>
-                  <q-chip square>
-                    <q-avatar rounded color="green" text-color="white">9</q-avatar>
-                    баллов
-                  </q-chip>
-                  <!--                    <span class="text-weight-bold">9/10</span>-->
-                </q-item-section>
-              </q-item>
               <q-expansion-item>
                 <template v-slot:header>
                   <q-item-section avatar>
-                    <q-avatar icon="videocam" color="primary" text-color="white"/>
+                    <q-avatar icon="movie_creation" color="primary" text-color="white"/>
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label lines="1">FFMPEG</q-item-label>
+                    <q-item-label caption lines="1">
+                      4 лабораторные работы по FFMPEG
+                    </q-item-label>
+                  </q-item-section>
+
+                  <!--                  <q-item-section side>
+                                      <q-chip square>
+                                        <q-avatar rounded color="orange" text-color="white">1</q-avatar>
+                                        балл
+                                      </q-chip>
+                                    </q-item-section>-->
+                </template>
+
+                <q-card>
+                  <q-card-section>
+                    <task-item
+                      v-for="c_id in ['197226351637', '247095020119', '247096586404', '253730218145', '285725197154']"
+                      :title="classroom_stats[c_id].title"
+                      :description="classroom_stats[c_id].description"
+                      :mark="classroom_stats[c_id].mark"
+                      :max_mark="classroom_stats[c_id].max_mark"
+                      :passed="classroom_stats[c_id].passed"
+                      type="classroom"
+                      :key="c_id"/>
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item>
+                <template v-slot:header>
+                  <q-item-section avatar>
+                    <q-avatar icon="videocam" color="grey" text-color="white"/>
                   </q-item-section>
 
                   <q-item-section>
@@ -50,59 +63,100 @@
 
                   <q-item-section side>
                     <q-chip square>
-                      <q-avatar rounded color="orange" text-color="white">1</q-avatar>
-                      балл
+                      <span class="text-subtitle">ожидается</span>
                     </q-chip>
                   </q-item-section>
                 </template>
 
                 <q-card>
                   <q-card-section>
-                    <q-item clickable v-ripple>
-                      <q-item-section avatar>
-                        <q-avatar color="red" text-color="white" icon="warning_amber"/>
-                      </q-item-section>
-
-                      <q-item-section>
-                        <q-item-label lines="1">ONVIF-1</q-item-label>
-                        <q-item-label caption lines="1">
-                          Непрерывное и абсолютное движение, фокус
-                        </q-item-label>
-                      </q-item-section>
-
-                      <q-item-section side>
-                        <q-chip square>
-                          <q-avatar rounded color="red" text-color="white">0</q-avatar>
-                          баллов
-                        </q-chip>
-                        <!--                    <span class="text-weight-bold">9/10</span>-->
-                      </q-item-section>
-                    </q-item>
-                    <q-item clickable v-ripple>
-                      <q-item-section avatar>
-                        <q-avatar color="red" text-color="white" icon="warning_amber"/>
-                      </q-item-section>
-
-                      <q-item-section>
-                        <q-item-label lines="1">ONVIF-2</q-item-label>
-                        <q-item-label caption lines="1">
-                          Настройки изображения (Imaging)
-                        </q-item-label>
-                      </q-item-section>
-
-                      <q-item-section side>
-                        <q-chip square>
-                          <q-avatar rounded color="red" text-color="white">0</q-avatar>
-                          баллов
-                        </q-chip>
-                        <!--                    <span class="text-weight-bold">9/10</span>-->
-                      </q-item-section>
-                    </q-item>
+                    <task-item
+                      title="ONVIF-1"
+                      description="Непрерывное и абсолютное движение, фокус"
+                      color="grey"
+                      custom_chip="ожидается"
+                      custom_icon="videocam"
+                    />
+                    <task-item
+                      title="ONVIF-2"
+                      description="Настройки изображения (Imaging)"
+                      color="grey"
+                      custom_chip="ожидается"
+                      custom_icon="videocam"
+                    />
+                    <task-item
+                      title="ONVIF-3"
+                      description="Вход/выход сигнализации и веселье"
+                      color="grey"
+                      custom_chip="ожидается"
+                      custom_icon="videocam"
+                    />
                   </q-card-section>
                 </q-card>
               </q-expansion-item>
+              <q-expansion-item>
+                <template v-slot:header>
+                  <q-item-section avatar>
+                    <q-avatar icon="work" color="orange" text-color="white"/>
+                  </q-item-section>
 
-              <q-separator inset="item"/>
+                  <q-item-section>
+                    <q-item-label lines="1">Треки</q-item-label>
+                    <q-item-label caption lines="1">
+                      Задания из треков
+                    </q-item-label>
+                  </q-item-section>
+
+                  <q-item-section side>
+                    <q-chip square>
+                      <span class="text-subtitle">{{ taiga_stats.score }} {{ taigaScoreTitle }}</span>
+                    </q-chip>
+                  </q-item-section>
+                </template>
+
+                <q-card>
+                  <q-card-section>
+                    <task-item
+                      title="Академический трек"
+                      description="Углубленные задания"
+                      type="taiga"
+                      :mark="taiga_stats.academic_score"
+                      custom_icon="assignment"
+                    />
+                    <task-item
+                      title="Прикладной трек"
+                      description="Актуальные профильные задачи"
+                      type="taiga"
+                      :mark="taiga_stats.application_score"
+                      custom_icon="construction"
+                    />
+                    <task-item
+                      title="Исследовательский трек"
+                      description="Научно-исследовательская работа"
+                      type="taiga"
+                      :mark="taiga_stats.research_score"
+                      custom_icon="library_books"
+                    />
+                    <task-item
+                      title="Эксплуатационный трек"
+                      description="Работа в медиацентре"
+                      type="taiga"
+                      :mark="taiga_stats.operational_score"
+                      custom_icon="camera_indoor"
+                    />
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <task-item
+                title="Итоговое задание"
+                description="Итоговое задание по курсу"
+                type="final_task"
+                color="grey"
+                custom_chip="ожидается"
+                custom_icon="assignment_ind"
+              />
+<!--              <q-separator/>-->
+
             </q-list>
           </q-card-section>
         </q-card>
@@ -147,108 +201,27 @@
 </template>
 
 <script>
+import TaskItem from "components/nvt/TaskItem";
+
 export default {
+  components: {
+    TaskItem
+  },
   data() {
     return {
       loading: true,
       loading_error: false,
-      tasks: [],
-      stats: {
-        "email": "aafyodorov@miem.hse.ru",
-        "taiga_score": {
-          "academic_score": 0,
-          "application_score": 0,
-          "operational_score": 12,
-          "research_score": 0,
-          "score": 12
-        },
-        "classroom_tasks": [
-          {
-            "task_name": "Аппаратные интерфейсы",
-            "task_id": "297686484826",
-            "mark": 100,
-            "max_mark": 100,
-            "passed": true
-          },
-          {
-            "task_name": "ffmpeg-4",
-            "task_id": "285725197154",
-            "mark": 10,
-            "max_mark": 10,
-            "passed": true
-          },
-          {
-            "task_name": "ffmpeg-2-1",
-            "task_id": "247095020119",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-3",
-            "task_id": "253730218145",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-2-2",
-            "task_id": "247096586404",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-1",
-            "task_id": "197226351637",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "Аппаратные интерфейсы",
-            "task_id": "297686484826",
-            "mark": 100,
-            "max_mark": 100,
-            "passed": true
-          },
-          {
-            "task_name": "ffmpeg-4",
-            "task_id": "285725197154",
-            "mark": 10,
-            "max_mark": 10,
-            "passed": true
-          },
-          {
-            "task_name": "ffmpeg-2-1",
-            "task_id": "247095020119",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-3",
-            "task_id": "253730218145",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-2-2",
-            "task_id": "247096586404",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          },
-          {
-            "task_name": "ffmpeg-1",
-            "task_id": "197226351637",
-            "mark": 0,
-            "max_mark": 10,
-            "passed": false
-          }
-        ]
-      }
+      classroom_stats: {},
+      taiga_stats: {},
+      stats: {}
+    }
+  },
+  computed: {
+    taigaScoreTitle() {
+      if (!this.taiga_stats || !this.taiga_stats.score)
+        return '';
+
+      return this.getTitle(this.taiga_stats.score, ["баллов", "балл", "баллов"])
     }
   },
   async mounted() {
@@ -256,15 +229,62 @@ export default {
   },
   methods: {
     async init() {
-
+      await this.getStats();
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000)
+    },
+    getTitle(number, titles) {
+      const cases = [2, 0, 1, 1, 1, 2];
+      return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
     },
     async getStats() {
       const endpoint = "https://constructor.auditory.ru/nvt-stats-api/user";
       const params = {
-        email: this.$q.sessionStorage.getItem('google_email');
+        email: this.$q.sessionStorage.getItem('google_email')
       }
       try {
         let res = await this.$axios.get(endpoint, {params: params});
+        this.stats = res.data
+        let classroom_tasks_info = {
+          "197226351637": {
+            title: "FFMPEG-1",
+            description: "Базовое кодирование видео",
+          },
+          "247095020119": {
+            title: "FFMPEG-2-1",
+            description: "Склейки",
+          },
+          "247096586404": {
+            title: "FFMPEG-2-2",
+            description: "Более веселые склейки",
+          },
+          "253730218145": {
+            title: "FFMPEG-3",
+            description: "Склейки с анимацией",
+          },
+          "285725197154": {
+            title: "FFMPEG-4",
+            description: "Синхронизация потоков с камеры и кодера",
+          },
+          "297686484826": {
+            title: "Аппаратные интерфейсы",
+            description: "Бонусный тест по материалам лекции",
+          }
+        }
+        this.stats.classroom_tasks.forEach((task) => {
+            let info = classroom_tasks_info[task['task_id']];
+            if (info !== undefined) {
+              info.mark = task.mark;
+              info.max_mark = task.max_mark;
+              info.task_name = task.task_name;
+              info.task_id = task.task_id;
+              info.passed = task.passed;
+            }
+          }
+        )
+        this.classroom_stats = classroom_tasks_info;
+        this.taiga_stats = this.stats.taiga_score;
         this.loading_error = false;
       } catch (e) {
         this.loading_error = true;
