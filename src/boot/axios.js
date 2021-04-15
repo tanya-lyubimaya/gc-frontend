518e05b7f7fb1412f76c39731eb35f6d9566151d
teshorygina <teshorygina@edu.hse.ru>
2021-04-15 12:40:01 +0300

    🩹 httpsAgent for instance in #562

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  withCredentials: true
-})
+  withCredentials: true,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
 
 Vue.prototype.$axios = axiosInstance;
 
-export {axiosInstance};
+export { axiosInstance };
