1961ffb92ac9f2011a604a7ad10148b8a97995df
teshorygina <teshorygina@edu.hse.ru>
2021-05-07 19:13:39 +0300

    🚧 Added video task to forms creation

--- src/pages/588/CreateForm.vue
+++ src/pages/588/CreateForm.vue
@@ -498,7 +498,7 @@ export default {
     videoLinkChosen() {
       this.step = 2;
       //this.video.sources[0].src = this.videoLink;
-      //this.video.sources[0].src = this.videoFile;
+      this.video.sources[0].src = this.videoFile;
     },
     getTimecode(index, i) {
       console.log(this.$refs.myPlayer[0].currentTime());
